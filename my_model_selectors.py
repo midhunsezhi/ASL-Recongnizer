2c6e1128a7d4d1ef3d86b2b159643705059e37a3
Midhun Dandamudi <midhunsezhi@gmail.com>
2017-05-26 14:01:31 -0700

    add Selector CV class

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -13,8 +13,8 @@ class ModelSelector(object):
     base class for model selection (strategy design pattern)
     '''
 
-    def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict, this_word: str,
-                 n_constant=3,
+    def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict,
+                 this_word: str, n_constant=3,
                  min_n_components=2, max_n_components=10,
                  random_state=14, verbose=False):
         self.words = all_word_sequences
@@ -103,6 +103,86 @@ class SelectorCV(ModelSelector):
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        this_word = self.this_word
+        sequences = self.sequences
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        if len(self.lengths) <= 1:
+            if self.verbose:
+                print("not enough sequences to train this word = {}"
+                      .format(this_word))
+            return None
+
+        hmm_models = {}
+
+        best_score_overall = None
+        best_i = None
+        fold = 0
+
+        n_fold_model_score = []
+        for i in range(self.min_n_components, self.max_n_components):
+
+            sum_logL = 0
+            best_score = None
+            best_model = None
+            num_of_models = 0
+
+            fold = 0
+            kf = KFold(n_splits=min(3, len(self.lengths)))
+
+            for cv_train_idx, cv_test_idx in
+            kf.split(sequences):
+
+                fold += 1
+
+                X, lengths = combine_sequences(cv_train_idx, word_sequences)
+                X2, lengths2 = combine_sequences(cv_test_idx, word_sequences)
+
+                try:
+                    model = GaussianHMM(n_components=n, covariance_type="diag",
+                                        n_iter=1000,
+                                        random_state=self.random_state,
+                                        verbose=False).fit(X, lengths)
+
+                    logL = model.score(X2, lengths2)
+                    sum_logL += logL
+                    num_of_models += 1
+                    if best_score is None or best_score < logL:
+                        best_score = logL
+                        best_model = model
+
+                    if self.verbose:
+                        print("model created : word = {}, number of states = {},  fold = {}, \
+                              score = {}".format(this_word, i, fold, logL))
+                except ValueError:
+                    if self.verbose:
+                        print("model failed to create : word = {}, \
+                               number of states = {}, score = {}"
+                              .format(this_word, i, fold))
+
+            if num_of_models == 0:
+                if self.verbose:
+                    print("no models generated for {} with {} states"
+                          .format(this_word, i))
+                continue
+
+            hmm_models[i] = best_model
+            # now average the scores for all the folds of this n
+            avg_logL = sum_logL / num_of_models
+            if self.verbose:
+                print("best score for word = {} with num of states = {}, \
+                      score = {}".format(this_word, i, best_score))
+
+                print("average score for word = {} with num of states = {}, \
+                      average score = {}".format(this_word, i, avg_logL))
+
+            if best_score_overall is None or best_score_overall < avg_logL:
+                best_score_overall = avg_logL
+                best_i = i
+
+        if self.verbose:
+            print("best model for {}, number of states = {}"
+                  .format(this_word, best_i))
+
+        if best_i is None:
+            return None
+        return hmm_models[best_i]
