ccbaa19e0b4741de4179093e85f04fd271f0fb24
Midhun Dandamudi <midhunsezhi@gmail.com>
2017-05-26 15:03:56 -0700

    implement recognizer

--- my_recognizer.py
+++ my_recognizer.py
@@ -1,5 +1,6 @@
 import warnings
 from asl_data import SinglesData
+import math
 
 
 def recognize(models: dict, test_set: SinglesData):
@@ -20,6 +21,23 @@ def recognize(models: dict, test_set: SinglesData):
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     probabilities = []
     guesses = []
-    # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+
+    for i in range(0, test_set.num_items):
+
+        local_probabilities = dict()
+        for word, model in models.items():
+            logL = -math.inf
+            X, lengths = test_set.get_item_Xlengths(i)
+            try:
+                if model is not None:
+                    logL = model.score(X, lengths)
+            except:
+                logL = -math.inf
+
+            local_probabilities[word] = logL
+
+        probabilities.append(local_probabilities)
+        best_guess = max(local_probabilities, key=local_probabilities.get)
+        guesses.append(best_guess)
+
+    return probabilities, guesses
